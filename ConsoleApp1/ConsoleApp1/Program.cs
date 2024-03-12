2b8ef89304180f44887fa0a2e5bf7e60d2201a0e
s26772 <s26772@pjwstk.edu.pl>
2024-03-12 11:25:28 +0100

    Modification 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("modification");
+Console.WriteLine("modification 1");
+Console.WriteLine("modification 2");
